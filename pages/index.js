e3d23c8c16cbe176e7eb4d12fba967677eeac248
Emotine69 <thaiduong.nguyen22@gmail.com>
2021-09-23 14:22:34 +0700

    second commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+        <Head>
+            <title>WebDev Newz</title>
+            <meta name='keywords' content='web development, programing'/>
+          </Head>
+        <h1>Welcome to Next</h1>
+    </div>
+  )
+}
